eca6ca6f70626c922de5454af839d5ea7e1abcfa
alexbogatov <66912711+alexbogatov@users.noreply.github.com>
2026-08-26 15:45:15 +0300

    Update worker.js Parallel poll_new_job and complete_job

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 // worker.js
 
-import os from 'os';
+import os from 'os'; 
 import { readFileSync, createReadStream } from 'fs';
 import { mkdir, writeFile, readdir, stat, unlink, rename } from 'fs/promises';
 import { join } from 'path';
